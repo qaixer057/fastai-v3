216b3ac4fefa6995962c89a1832a8d16dfff3739
Qaiser Abbas <mqaiser617@gmail.com>
2020-03-10 14:56:13 +0500

    Update server.py

--- app/server.py
+++ app/server.py
@@ -12,7 +12,7 @@ from starlette.staticfiles import StaticFiles
 export_file_url = 'https://drive.google.com/uc?export=download&id=1-0FzjyCi_QumkpiBZCP5TXvjaqoGROo5'
 export_file_name = 'export.pkl'
 
-classes = ['acral', 'benign']
+classes = ['Acral Melanoma', 'Benign Nevus']
 path = Path(__file__).parent
 
 app = Starlette()
